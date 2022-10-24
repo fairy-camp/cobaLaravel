6c0c6bb3061ad27037b8b2337ba847cb29cb82af
fairy-camp <arifpsp0507@gmail.com>
2022-10-24 12:53:00 +0700

    ui

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,22 +5,33 @@
 <h1 class="mb-5">{{ $title }}</h1>
 
 @if ($posts->count())
-    <div class="card mb-3">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
-                This content is a little bit longer.</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    <div class="card mb-5">
+        <img src="https://source.unsplash.com/1200x400?{{ $posts[0]->category->name }}" class="card-img-top" alt="...">
+        <div class="card-body text-center">
+            <h3 class="card-title"><a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h3>
+            <small class="text-muted">
+                <p>By <a href="/authors/{{ $posts[0]->author->username }}" class="text-decoration-none"> {{ $posts[0]->author->name   }}</a> in <a href="/categories/{{ $posts[0]->category->slug }}" class="text-decoration-none">{{ $posts[0]->category->name }}</a>
+                {{ $posts[0]->created_at->diffForHumans() }}</p>
+            </small>   
+            <p class="card-text">{{ $posts[0]->excert }}</p>
+
+            <a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none btn btn-primary">Read more</a>
+             
         </div>
     </div>
 @else
     <p class="text-center fs-4">No post found</p>    
 @endif
 
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
 
+        </div>
+    </div>
+</div>
 
-@foreach ($posts as $post)
+@foreach ($posts->skip(1) as $post)
 <article class="mb-5 border-bottom pb-4">
     <h2>
         <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
